d02af31f993b9c0550cf018664f99d2e897a8783
Marat <zinatulinmarat@gmail.com>
2023-06-30 23:51:06 +0500

    next commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IParticipant } from './interfaces/participant.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'loblab';
+  participantData: IParticipant[] = [];
+  baseProbability = 0.7;
+  winnerExists = false;
+
+  addParticipant(): void {
+    const id = this.participantData.length + 1;
+    const name = `Participant - ${id}`;
+    const isWin = this.defineWinner();
+    this.participantData.push({id, name, isWin});
+  }
+
+  defineWinner(): boolean {
+    if (this.winnerExists) {
+      return false;
+    }
+    const probability = this.getProbability();
+    const randomNumber = Math.random();
+    if (probability > randomNumber) {
+      this.winnerExists = true;
+      return true;
+    }
+    return false;
+  }
+
+  getProbability(): number {
+    return this.baseProbability / (this.participantData.length + 1);
+  }
 }
